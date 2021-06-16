9d2e22ae5a855e9cbbfb375aa96792f9ad6caa77
PedroHumberto <pedrohlucape@yahoo.com.br>
2021-06-16 20:55:53 -0300

    Add project files.

---
+++ CadastroDeAlunoComConstrutores/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace CadastroDeAluno {
+    class Program {
+        static void Main(string[] args) {
+
+           
+            // entrada temporaria de dados para depois ser substituida pelo Student a, onde vai receber todos os valores citados
+            // toranando assim a entrada inicial dos dados obrigatoria
+            Console.Write("Student Name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Type three notes:");
+            double note1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double note2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double note3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Student a = new Student(name, note1, note2, note3);
+
+            Console.WriteLine("Final Note = " + a.FinalResult().ToString("F2", CultureInfo.InvariantCulture));
+            if (a.Aproved()) {
+                Console.WriteLine("Aproved");
+            }
+
+
+            else {
+                Console.WriteLine("Reproved");
+
+                Console.WriteLine("You need: "
+                    + a.RestNote().ToString("F2", CultureInfo.InvariantCulture)
+                    + " points");
+            }
+
+
+        }
+    }
+}
---
+++ CadastroDeAlunoComConstrutores/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace CadastroDeAluno {
+    class Student {
+
+        public string Name;
+        public double Note1, Note2, Note3;
+        
+        
+        //Construtor, é adicionado as informações para depois dar os valores a elas
+        public Student(string name, double note1, double note2, double note3) {
+            Name = name;
+            Note1 = note1;
+            Note2 = note2;
+            Note3 = note3;
+        }
+
+        public double FinalResult() {
+            return Note1 + Note2 + Note3;
+        }
+
+        public bool Aproved() {
+            if (FinalResult() >= 60.0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        public double RestNote() {
+            if (Aproved()) {
+                return 0.0;
+            }
+            else {
+                return 60.0 - FinalResult();
+            }
+        }
+
+    }
+
+}
